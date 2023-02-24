f9b14b0e5a294d59974c884a720b852094970a55
rosumin <ujou173@gmail.com>
2023-02-24 15:40:57 +0900

    사칙연산 계산기 구현 완료

--- aaa.js
+++ aaa.js
@@ -6,8 +6,39 @@ for(i = 0 ; i < pokemon.length; i++) {
   console.log(pokemon[i]);
 }
 
-function bmiCalculateReturnString(myWeight, myHeight) {
-  let bmi = myWeight / (myHeight * myHeight);
-  return "BMI = " + bmi + "Kg/m^2";
+// bmi 계산기
+function bmiCalculateReturnString(myWeight, myHeight) { //몸무게와 키를 매개변수로 한다
+  let bmi = myWeight / ((myHeight * 0.01) * (myHeight * 0.01)); // 몸무게 / 키^2을 계산. 우리는 평소 키를 cm 단위로 자주 사용하므로 m로 변환을 위해 0.01을 곱해준다
+  return "BMI = " + bmi + "Kg/m^2"; // 계산 결과를 반환한다
 }
-console.log(bmiCalculateReturnString(70, 1.68));
+console.log(bmiCalculateReturnString(70, 168)); //70kg을 몸무게로, 168cm를 키로 해서 계산
+
+// 부가가치세 계산기
+function taxCalculateReturnString(price) {
+  let vat = price * 10 / 100;
+  return "V.A.T = " + vat + "원" 
+}
+console.log(taxCalculateReturnString(3700));
+
+// 사칙연산 계산기
+function numberCalculate(firstValue, calc, secondValue) {
+  let result;
+  if(calc === "+") {
+    result = firstValue + secondValue;
+    return result;
+  } else if(calc === "-") {
+    result = firstValue - secondValue;
+    return result;
+  } else if (calc === "x") {
+    result = firstValue * secondValue;
+    return result;
+  } else if (calc === "/") {
+    result = firstValue / secondValue;
+    return result;
+  } else {
+    result = "계산 부호가 정확하지 않습니다";
+    return result;
+  }
+}
+console.log("------")
+console.log(numberCalculate(27, 1, 3));
